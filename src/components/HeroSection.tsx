e50ecbb82db10676573e4ad76a13e6cc0aa10f15
Qasim <mianqasim065@gmail.com>
2025-11-19 18:44:12 +0500

    style the hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,8 +5,10 @@ export default function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative">
       <div className="p-4 relative z-10 w-full text-center">
-        <h1>Mater the art of music</h1>
-        <p>
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Mater the art of music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Dive into our comprehensive music courses and transform your musical
           journey today. Whether you're a beginner or looking to refine your
           skills, join us to unlock your true potential
