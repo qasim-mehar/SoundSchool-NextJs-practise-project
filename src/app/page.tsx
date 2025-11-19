bcb7fc1b84735f21d8aff042b7409667d757f6d3
Qasim <mianqasim065@gmail.com>
2025-11-20 00:23:46 +0500

    feat: add FeaturedCourses section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { h1, main } from "motion/react-client";
 import Image from "next/image";
+
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
