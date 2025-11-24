190a19c856182a1965926ccce7df012b501ae5e4
Qasim <mianqasim065@gmail.com>
2025-11-24 19:15:34 +0500

    feat: add wavy background in intructor section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { StickyScroll } from "./components/ui/sticky-scroll-reveal";
 import { WhyChooseUs } from "./components/WhyChooseUs";
 import { TestimonialCards } from "./components/TestimonialCards";
 import UpcomingWebinars from "./components/UpcomingWebinars";
-import { AnimatedTooltipPreview } from "./components/AnimatedTooltipPreview";
+import { Instructors } from "./components/Instructors";
 
 export default function Home() {
   return (
@@ -17,8 +17,8 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <TestimonialCards />
+      <Instructors />
       <UpcomingWebinars />
-      <AnimatedTooltipPreview />
     </main>
   );
 }
